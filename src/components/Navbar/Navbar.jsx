9a65e3dcdb8606ccfce7b4d301a422757b522278
ranjithk0701 <missionmoneyheist700@gmail.com>
2025-03-14 21:09:30 +0530

    added home page redirection

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 // import { useContext } from "react";
 // import { CurrencyContext } from "../../context/CurrencyContext";
+import { useNavigate } from 'react-router-dom';
 import currencyStore from '../../state/store';
 
 function Navbar() {
 
     // const {setCurrency} = useContext(CurrencyContext);
     const {setCurrency} = currencyStore();
+
+    const navigate = useNavigate();
+    function goToHome() {
+        navigate('/');
+    }
+
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="navbar-start">
@@ -21,10 +28,10 @@ function Navbar() {
                 </ul>
                 </div>
             </div>
-            <div className="navbar-center">
+            <div onClick={goToHome} className="navbar-center">
                 <a className="btn btn-ghost text-xl">Crypto Tracker</a>
             </div>
-            <div className="navbar-end">
+            <div  className="navbar-end">
                 <button className="btn btn-ghost btn-circle">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
                 </button>
